9dba6da361b3b1ff7e273b8e67fb8cb2c8b174bb
Melvin Zottola <mzottola@qovery.com>
2026-02-17 10:39:00 +0100

    chore: Enable wrapping and disable copy into clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Enable wrapping
+local opt = vim.opt
+opt.wrap = true
+
+-- Disable auto copy into clipboard
+opt.clipboard = ""
